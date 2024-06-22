99c31dd105dee00035804b59c898c49f234bb6bd
DTAJ095 <albanjaures26@gmail.com>
2024-06-22 17:58:36 +0100

    added

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -36,5 +36,7 @@ class Server:
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """ Return a tuple of size two containing a start index and an end index """
+    """ Return a tuple of size two containing a start
+    index and an end index
+    """
     return ((page - 1) * page_size, page * page_size)
