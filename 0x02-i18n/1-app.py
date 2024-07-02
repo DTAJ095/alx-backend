cefc5a3b506981995cf12e19befea272a28a98ec
DTAJ095 <albanjaures26@gmail.com>
2024-07-03 00:41:31 +0100

    added

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,6 +3,9 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
+app = Flask(__name__)
+babel = Babel(app)
+
 
 class Config():
     """ Config class """
@@ -10,10 +13,9 @@ class Config():
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-app = Flask(__name__)
-babel = Babel(app)
+
 app.config.from_object(Config)
-#app.url_map.strict_slashes = False
+app.url_map.strict_slashes = False
 
 
 @app.route('/', strict_slashes=False)
