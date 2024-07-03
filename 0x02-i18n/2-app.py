79557debfa7b90c282f34d325f2921a095cc0e1d
DTAJ095 <albanjaures26@gmail.com>
2024-07-03 15:23:28 +0100

    corrected

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -10,7 +10,7 @@ babel = Babel(app)
 class Config():
     """ Configuration class """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE  = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
     DEBUG = True
 
