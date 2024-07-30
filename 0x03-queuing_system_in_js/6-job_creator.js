dda26a4fcb0fe2a1dfbd941d206f0698291f9d9f
DTAJ095 <albanjaures26@gmail.com>
2024-07-30 07:34:27 -0700

    corrected

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -5,18 +5,18 @@ import Keu from 'kue';
 
 const queue = Keu.createQueue();
 const jobData = {
-    phoneNumber: '+237690725411',
-    message: 'Hello, this is a test message!'
+  phoneNumber: '+237690725411',
+  message: 'Hello, this is a test message!'
 };
 
 const job = queue.create('push_notification_code', jobData).save((err) => {
-    if (!err) console.log(`Notification job created: ${job.id}`);
+  if (!err) console.log(`Notification job created: ${job.id}`);
 });
 
 job.on('complete', () => {
-    console.log('Notification job completed');
+  console.log('Notification job completed');
 });
 
 job.on('failed', () => {
-    console.log('Notification job failed');
+  console.log('Notification job failed');
 });
