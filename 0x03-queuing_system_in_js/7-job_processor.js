95159b1266bdf11edcb4eb76e6787d80e6ec0c46
DTAJ095 <albanjaures26@gmail.com>
2024-07-30 07:56:17 -0700

    okay

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -6,24 +6,24 @@ const queue = Keu.createQueue();
 const blackList = ['4153518780', '4153518781'];
 
 /**
- * 
+ *
  * @param {string} phoneNumber - phone number
  * @param {string} message - push notification message
  * @param {import ('kue').job} job - queue job
  * @param {import('kue').DoneCallback} done - callback
  * @returns {void}
  */
-function sendNotification(phoneNumber, message, job, done) {
-    job.progress(0, 100);
-    if (blackList.includes(phoneNumber)) {
-        done(Error(`Phone number ${phoneNumber} is blacklisted`));
-        return;
-    }
-    job.progress(50, 100);
-    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-    done();
+function sendNotification (phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blackList.includes(phoneNumber)) {
+    done(Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
 }
 
 queue.process('push_notification_code', (job, done) => {
-    sendNotification(job.data.phoneNumber, job.data.message, job, done);
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
 });
--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,22 +1,22 @@
 // Writing the job creation function
 
-export default function createPushNotificationsJobs(jobs, queue) {
-    if (!Array.isArray(jobs)) throw Error('Jobs is not an array');
-    jobs.forEach((jobData) => {
-        const job = queue.create('push_notification_code', jobData).save((err) => {
-            if (!err) console.log(`Notification job created: ${job.id}`);
-        });
-
-        job.on('complete', () => {
-            console.log(`Notification job ${job.id} completed`);
-        });
-
-        job.on('failed', () => {
-            console.log(`Notification job ${job.id} failed`);
-        });
-
-        job.on('progress', (progress) => {
-            console.log(`Notification job ${job.id} ${progress}% complete`);
-        });
+export default function createPushNotificationsJobs (jobs, queue) {
+  if (!Array.isArray(jobs)) throw Error('Jobs is not an array');
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code', jobData).save((err) => {
+      if (!err) console.log(`Notification job created: ${job.id}`);
     });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', () => {
+      console.log(`Notification job ${job.id} failed`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
 }
