f4d093bb854db31d016d94deb8542e62aa5d68eb
DTAJ095 <albanjaures26@gmail.com>
2024-07-30 07:42:57 -0700

    okay

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -5,14 +5,14 @@ const queue = Keu.createQueue();
 
 /**
  * Send a notification to a given phone number
- * @param {string} phoneNumber 
- * @param {string} message 
+ * @param {string} phoneNumber
+ * @param {string} message
  */
-function sendNotification(phoneNumber, message) {
-    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+function sendNotification (phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
 queue.process('push_notification_code', (job, done) => {
-    sendNotification(job.data.phoneNumber, job.data.message);
-    done();
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
 });
